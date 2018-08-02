ffdfb9d43070b701274f5fa0a99f9f4507ac60dc
aandrosov <coffee-04@yandex.ru>
2018-08-02 08:42:33 +0300

    added model classes

---
+++ src/main/scala/com/catran/trading/model/Candle.scala
@@ -0,0 +1,16 @@
+package com.catran.trading.model
+
+/**
+  * Created by Administrator on 7/30/2018.
+  */
+class Candle(
+                   ticker: String,
+                   timestamp: String,
+                   open: Double,
+                   high: Double,
+                   low: Double,
+                   close: Double,
+                   volume: Long
+                 ) {
+
+}
---
+++ src/main/scala/com/catran/trading/model/Teak.scala
@@ -0,0 +1,13 @@
+package com.catran.trading.model
+
+/**
+  * Created by Administrator on 7/30/2018.
+  */
+case class Teak(
+                 timestamp: String,
+                 ticker: String,
+                 price: Double,
+                 volume: Long
+               ) {
+
+}
