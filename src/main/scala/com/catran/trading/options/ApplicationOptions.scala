a55f8fa4005d68573ad77d699ae2ede6503b31c9
BARSUM\aandrosov <coffee-04@yandex.ru>
2018-08-02 21:33:10 +0300

    added options

---
+++ src/main/scala/com/catran/trading/options/ApplicationOptions.scala
@@ -0,0 +1,50 @@
+package com.catran.trading.options
+
+import com.catran.trading.netty.client.{ClientHandler, TeakHandler}
+import io.netty.channel.ChannelHandler
+
+/**
+  * using for application the different options
+  */
+case class ApplicationOptions(
+                               storage: String = "SqLite",
+                               tableName: String = "teakTable",
+                               brokerHost: String = "localhost",
+                               brokerPort: Int = 5555,
+                               serverHost: String = "localhost",
+                               serverPort: Int = 5554,
+                               clientHost: String = "localhost",
+                               clientPort: Int = 5554,
+                               brokerHandler: ChannelHandler = new TeakHandler(),
+                               clientHandler: ChannelHandler = new ClientHandler(),
+                               sqlite: SqLiteOptions = SqLiteOptions()
+                             )
+
+object ApplicationOptions {
+  val defaults = new ApplicationOptions()
+
+  /**
+    * Initialize options with given arguments
+    */
+  def apply(args: Array[String]): ApplicationOptions = {
+    if (args.isEmpty) return ApplicationOptions.defaults
+    args.foldLeft(ApplicationOptions()) { (options, arg) =>
+      arg.split("=") match {
+        case Array("storage", value) => options.copy(storage = value)
+        case Array("tableName", value) => options.copy(tableName = value)
+
+        case Array("sqlite.workingConnection", value) => options.copy(sqlite = options.sqlite.copy(workingConnection = value))
+        case Array("sqlite.testConnection", value) => options.copy(sqlite = options.sqlite.copy(testConnection = value))
+        case Array("sqlite.driver", value) => options.copy(sqlite = options.sqlite.copy(driver = value))
+
+        case exc => throw new RuntimeException(s"invalid args: ${exc.mkString}")
+      }
+    }
+  }
+}
+
+case class SqLiteOptions(
+                          workingConnection: String = "jdbc:sqlite:userdb.db",
+                          testConnection: String = "jdbc:sqlite:testdb.db",
+                          driver: String = "org.sqlite.JDBC"
+                        )
